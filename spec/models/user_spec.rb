ae8bda473a15d7e1ce431b0de653fda9fe40bc2d
SHG9-hub <zhuanzhitailang74@gmail.com>
2025-05-25 15:43:59 +0900

    [add]RSpecテストを整備し、Devise認証・モデル基本テストを追加。Userモデル、Attendanceモデル、AttendancesControllerに対し、現在の実装に合わせた基本的なRSpecテストを追加・修正。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it "メールアドレスがない場合は無効であること" do
+      user = User.new(email: nil, password: "password")
+      expect(user).to_not be_valid
+    end
 
-  it "メールアドレスがない場合は無効であること" do
-    user = User.new(email: nil)
-    expect(user).to_not be_valid
+    it "メールアドレスがある場合は有効であること" do
+      user = User.new(email: "test@example.com", password: "password")
+      expect(user).to be_valid
+    end
+
+    it "重複するメールアドレスは無効であること" do
+      User.create!(email: "test@example.com", password: "password")
+      user = User.new(email: "test@example.com", password: "password")
+      expect(user).to_not be_valid
+    end
+  end
+
+  describe 'アソシエーション' do
+    it "attendancesアソシエーションを持つこと" do
+      user = User.new
+      expect(user).to respond_to(:attendances)
+    end
+  end
+
+  describe 'Devise設定' do
+    it "メールアドレスでログインできること" do
+      user = User.create!(email: "test@example.com", password: "password")
+      expect(user.valid_password?("password")).to be true
+    end
+
+    it "間違ったパスワードでは認証に失敗すること" do
+      user = User.create!(email: "test@example.com", password: "password")
+      expect(user.valid_password?("wrongpassword")).to be false
+    end
   end
 end
