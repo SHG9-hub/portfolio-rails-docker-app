e88842394284fe1ac6f80ee3e2cb6a9d42697d7b
SHG9-hub <zhuanzhitailang74@gmail.com>
2025-05-25 17:44:32 +0900

    [add]ユーザーがログイン後、出勤時間を記録し、一覧ページで確認できる機能の追加

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,26 @@
 class AttendancesController < ApplicationController
     before_action :authenticate_user!
     def index
-        
+        @attendances = current_user.attendances.order(check_in: :desc)
+    end
+
+    def new
+        @attendance = Attendance.new
     end
     
     def create
-        
+        @attendance = current_user.attendances.build(attendance_params)
+        if @attendance.save
+          redirect_to attendances_path
+        else
+          flash.now[:alert] = @attendance.errors.full_messages.join(', ')
+          render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def attendance_params
+        params.require(:attendance).permit(:check_in)
     end
 end
