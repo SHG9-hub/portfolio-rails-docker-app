672a41136e27c11da55f9836b67b5e61f1267660
SHG9-hub <zhuanzhitailang74@gmail.com>
2025-05-25 18:39:39 +0900

    [add]退勤時間を記録する機能を追加しました。

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -18,9 +18,23 @@ class AttendancesController < ApplicationController
         end
     end
 
+    def edit
+        @attendance = current_user.attendances.find(params[:id])
+    end
+
+    def update
+        @attendance = current_user.attendances.find(params[:id])
+        if @attendance.update(attendance_params.merge(check_out: Time.current))
+            redirect_to attendances_path
+        else
+            flash.now[:alert] = @attendance.errors.full_messages.join(', ')
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def attendance_params
-        params.require(:attendance).permit(:check_in)
+        params.require(:attendance).permit(:check_in, :check_out)
     end
 end
