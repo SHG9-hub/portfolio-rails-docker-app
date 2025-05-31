75dd76a6a4735ae8e686f8efc7b87e7c0ef01b7b
SHG9-hub <zhuanzhitailang74@gmail.com>
2025-05-31 14:38:52 +0900

    [add]勤怠レコードの削除機能を追加。勤怠レコードを一覧画面から削除できるようにしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :attendances, only: [:index, :new, :create, :edit, :update]
+  resources :attendances, only: [:index, :new, :create, :edit, :update, :destroy]
   root to: "attendances#index"
   get "up" => "rails/health#show", as: :rails_health_check
 end
